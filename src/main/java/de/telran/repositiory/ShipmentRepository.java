e3180ff3ef24403c57f6fb8e5818de02ee524bac
Evgenygroup <mailfrom61@gmail.com>
2020-04-09 14:15:58 +0200

    adding 'get all customer's shipments by customer id' method

--- src/main/java/de/telran/repositiory/ShipmentRepository.java
+++ src/main/java/de/telran/repositiory/ShipmentRepository.java
@@ -4,5 +4,9 @@ import de.telran.entity.Customer;
 import de.telran.entity.Shipment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ShipmentRepository extends JpaRepository<Shipment,Long> {
+
+    List<Shipment> findAllShipmentsByCustomerId(Long CustomerId);
 }
